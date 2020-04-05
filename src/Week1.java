ede19e181dccd97c99bd1808254f1433fc112a8e
Jammersg <james.alexander.gallaher@gmail.com>
2020-04-04 18:11:25 -0700

    first commit

---
+++ src/Week1.java
@@ -0,0 +1,34 @@
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double apple = 1.99;
+		int wallet = 220;
+		int friends = 5;
+		int age = 24;
+		String firstName = "Sam";
+		String lastName = "Crow";
+		char middle = 'M';
+		double remaining = wallet - apple;
+		String balance = "The remaining balance is: ";
+		String friendsMade = "After I won the lottery when I was ";
+		String friendsMade2 = ", my friend list increased by ";
+		
+		
+		
+		System.out.println(balance + remaining);
+		
+		
+		System.out.println(friendsMade + age + friendsMade2 + friends + ".");
+		
+
+		System.out.println(firstName + " " + middle + " " + lastName);
+		
+		
+		
+
+	}
+
+}
